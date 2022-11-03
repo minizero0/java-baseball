2768fd2f15aa151ed73f5950b81ee0b5472aa385
yeongmin <dudals1069@gmail.com>
2022-11-04 01:24:09 +0900

    strikeCheck,ballCheck

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,49 @@ import java.util.Scanner;
 public class Application {
     static Scanner sc = new Scanner(System.in);
 
+    //스트라이크 개수 체크
+    public static String strikeCheck(int cnt){
+        String str;
+        switch (cnt){
+            case 1:str="1스트라이크";break;
+            case 2:str="2스트라이크";break;
+            case 3:str="3스트라이크";break;
+            default:str=null;
+        }
+        return str;
+    }
+
+    //볼 개수 체크
+    public static String ballCheck(int cnt){
+        String str;
+        switch (cnt){
+            case 1:str="1볼";break;
+            case 2:str="2볼";break;
+            case 3:str="3볼";break;
+            default:str=null;
+        }
+        return str;
+    }
+
+    public static String numberCheck(int[] number, List<Integer> randNumber){
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < number.length; i++){
+            if(number[i]==randNumber.get(i))
+                cnt1++;
+            else if(randNumber.contains(number[i]))
+                cnt2++;
+        }
+        String str1 = strikeCheck(cnt1);
+        String str2 = ballCheck(cnt2);
+        if(str1==null&&str2==null)
+            return "낫싱";
+        else if(str2==null)
+            return str1;
+        else
+            return str2 + " " + str1;
+    }
+
     //입력받은 정수 배열로 변환
     public static int[] numberTrans(int number){
         int arr[] = new int[3];
@@ -38,8 +81,16 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> randNumber = randNumber();
+        System.out.println(randNumber);
         System.out.print("숫자를 입력해주세요 : ");
         int number = sc.nextInt();
+        int arr[] = numberTrans(number);
+        String str = numberCheck(arr, randNumber);
+        System.out.println(str);
+        if (str.equals("3스트라이크")){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
 
 
 
